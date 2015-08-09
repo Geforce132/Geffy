b1dfd4e993ba27854faf96804a01f6b956059214
Geforce132 <Geforce132@gmail.com>
2015-08-08 18:46:09 -0700

    -Re-added channels Geffy joins. -Changed SecurityCraft version.

--- src/org/freeforums/geforce/geffy/main/Reference.java
+++ src/org/freeforums/geforce/geffy/main/Reference.java
@@ -11,10 +11,10 @@ public class Reference {
 	public static final String commandPrefix = "~";
 	
 	public static ICommand[] commands = new ICommand[]{new CommandHelp(), new CommandRate(), new CommandGiveCookie()};
-    public static String[] joinedChannels = new String[]{"#GeforceMods"/*, "#GeforceMods", "#bl4ckscor3", "#Razz"*/};	
+    public static String[] joinedChannels = new String[]{"#GeforceMods", "#bl4ckscor3", "#Razz"};	
     public static String[] ignoredUsers = new String[]{"GeffyBot", "Geffy"};	
     
     //Other things.
 	public static String[] cookieTypes = new String[]{"chocolate chip", "peanut butter", "raisin", "oatmeal", "white fudge", "sugar", "frosted", "Oreo"};
-	public static String scVersion = "v1.8.0"; //TODO: Change this with every SecurityCraft update
+	public static String scVersion = "v1.7.4.1"; //TODO: Change this with every SecurityCraft update
 }
